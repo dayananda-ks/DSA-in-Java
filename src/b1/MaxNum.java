f0849a61227ab2cb32696b6cea38e0b62a8ebf3d
Dayananda KS <dayanandaks045@gmail.com>
2026-05-10 12:34:55 +0530

    change letters cases

--- src/b1/MaxNum.java
+++ src/b1/MaxNum.java
@@ -4,12 +4,12 @@ public class MaxNum {
     public static void main(String[] args) {
         
         int[] array = {4,7,5,18,3};
-        int Max = array[0];
+        int max = array[0];
         for(int index = 0; index < array.length; index++){
-            if(array[index] > Max){
-                Max = array[index];
+            if(array[index] > max){
+                max = array[index];
             }  
         }
-        System.out.println("Maximum number in a array is : " + Max);
+        System.out.println("Maximum number in a array is : " + max);
     }
 }
---
+++ src/b1/MinNum.java
@@ -0,0 +1,15 @@
+package b1;
+
+public class MinNum {
+    public static void main(String[] args) {
+
+        int[] array = {4,7,5,18,3};
+        int min = array[0];
+        for(int index = 0; index < array.length; index++){
+            if(array[index] > min){
+                min = array[index];
+            }
+        }
+        System.out.println("Minimum number in a array is : " + min);
+    }
+}
