56ffe324aa1d1097908286098b17776eaf40ca03
Dayananda KS <dayanandaks045@gmail.com>
2026-05-10 12:11:40 +0530

    first commit

---
+++ src/LinearTraversal/LinearTraversal.java
@@ -0,0 +1,12 @@
+package LinearTraversal;
+public class LinearTraversal {
+    public static void main(String[] args) {
+        int[] array = {4,7,5,18,3};
+        int Max = array[0];
+        for(int index = 0; index < array.length; index++){
+            if(array[index] > Max){
+                Max = array[index];
+            }
+        }System.out.println("Maximum number in a array is :" + Max);
+    }
+}
---
+++ src/LinearTraversal/LinearTraversal.md
@@ -0,0 +1,2 @@
+### Linear Traversal
+Linear traversal is a fundamental technique in computer science that involves visiting each element of a data structure sequentially. This method is commonly used in arrays, linked lists, and other linear data structures. The primary purpose of linear traversal is to access or manipulate each element in a specific order, typically from the first to the last.
