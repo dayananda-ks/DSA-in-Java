a9093ebdd2bb8439d32be337347adcdaabee9eb8
Dayananda KS <dayanandaks045@gmail.com>
2026-06-11 18:34:01 +0530

    Update array practice solutions

--- src/ArraysBasic/MinNum.java
+++ src/ArraysBasic/MinNum.java
@@ -1,9 +1,13 @@
 package ArraysBasic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinNum {
     public static void main(String[] args) {
 
         int[] array = {4,7,5,18,3};
+        Map<Integer,Integer> map = new HashMap<>();
         int min = array[0];
         for(int index = 0; index < array.length; index++){
             if(array[index] < min){
