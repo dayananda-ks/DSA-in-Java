15815e3ab2dfdd8bcd2de523ab9a651d83872beb
Dayananda KS <dayanandaks045@gmail.com>
2026-07-08 09:44:33 +0530

    Add Perfect Number solution

---
+++ src/NumbersProblems/PerfectNumber.java
@@ -0,0 +1,25 @@
+package NumbersProblems;
+import java.util.Scanner;
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        if (num <= 0) {
+            System.out.print("Invalid Number");
+            return;
+        }else {
+            int sum = 0;
+            for (int i = 1; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    sum += i;
+                }
+            }
+            if (sum == num) {
+                System.out.println("Perfect Number");
+            } else {
+                System.out.println("Not Perfect Number");
+            }
+        }
+    }
+}
