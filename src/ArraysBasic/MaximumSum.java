e9009b962da1836de3ea9f34e701a3c322ea5dc2
Dayananda KS <dayanandaks045@gmail.com>
2026-07-08 09:17:02 +0530

    Add Java DSA solutions

--- src/ArraysBasic/MaximumSum.java
+++ src/ArraysBasic/MaximumSum.java
@@ -2,15 +2,12 @@ package ArraysBasic;
 
 import java.util.ArrayList;
 
-
-
-
 public class MaximumSum {
     public static void main(String[] args){
         double a = 1200;
         double sum = ((a/2)+(a/3)+(a/4));
         System.out.println(sum);
-        System.out.println((int)sum);
+
 //        double calc = ((Math.floor(n1)) + Math.floor(n2) + Math.floor(n3));
 //        System.out.println(calc);
 //3.5 + 2.3 + 1.75
