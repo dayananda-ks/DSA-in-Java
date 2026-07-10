59e73e0afdc14de8cd8cadc4171b520b2a31d96f
Dayananda KS <dayanandaks045@gmail.com>
2026-07-10 13:02:07 +0530

    Add HollowRightAngledTriangle solution

--- src/PatternProblems/HollowRightAngledTriangle.java
+++ src/PatternProblems/HollowRightAngledTriangle.java
@@ -1,4 +1,20 @@
 package PatternProblems;
 
+import java.util.Scanner;
+
 public class HollowRightAngledTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= i; j++){
+                if( (j == 1) || (i == j) || (i == rows)){
+                    System.out.print("*" +" ");
+                }else{
+                    System.out.print("  ");
+                }
+            }System.out.println();
+        }
+    }
 }
