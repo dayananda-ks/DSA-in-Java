fa4022250f67b108298e90589fbd46554c4f07a8
Dayananda KS <dayanandaks045@gmail.com>
2026-07-10 19:27:40 +0530

    Add ButterflyPattern solution

--- src/PatternProblems/ButterflyPattern.java
+++ src/PatternProblems/ButterflyPattern.java
@@ -1,4 +1,35 @@
 package PatternProblems;
 
+import java.util.Scanner;
+
 public class ButterflyPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for(int j = rows - 1; j >= i; j-- ) {
+                System.out.print("    ");
+            }
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <=rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print("* ");
+            }
+            for(int j = 1; j <= i; j++) {
+                System.out.print("    ");
+            }
+            for(int j = rows; j >i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
