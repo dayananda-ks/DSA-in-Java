e240c21d23469be12fca9b31b12c4d66ba66f5e4
Dayananda KS <dayanandaks045@gmail.com>
2026-07-10 13:03:42 +0530

    Add Triangle01 solution

--- src/PatternProblems/Triangle01.java
+++ src/PatternProblems/Triangle01.java
@@ -1,4 +1,17 @@
 package PatternProblems;
 
+import java.util.Scanner;
+
 public class Triangle01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for(int i = 1; i <= rows;i++){
+            for(int j = 0; j < i; j++){
+                int temp = (i+j) % 2 ;
+                System.out.print(temp + " ");
+            }System.out.println();
+        }
+    }
 }
