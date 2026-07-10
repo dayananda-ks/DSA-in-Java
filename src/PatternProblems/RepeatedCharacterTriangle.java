33c492852ca91bf4ab2aad7a95edeae1deace541
Dayananda KS <dayanandaks045@gmail.com>
2026-07-10 13:03:10 +0530

    Add RepeatedCharacterTriangle solution

--- src/PatternProblems/RepeatedCharacterTriangle.java
+++ src/PatternProblems/RepeatedCharacterTriangle.java
@@ -1,4 +1,17 @@
 package PatternProblems;
 
+import java.util.Scanner;
+
 public class RepeatedCharacterTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char)('A' + i));
+            }
+            System.out.println();
+        }
+    }
 }
